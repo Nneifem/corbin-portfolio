7d5783847df5cde84167a7058df823a11bd7379c
Nneif <nneifemakpunonu@gmail.com>
2023-11-06 13:13:31 -0500

    updated the youtube section

--- src/pages/Youtube.jsx
+++ src/pages/Youtube.jsx
@@ -1,11 +1,16 @@
 import  '../assets/images/styles/Youtube.css';
+import thumbnail from '../assets/images/thumbnail.jpg';
 
 export default function Youtube (){
     return(
         <>
             <h1 className='youtube-title'>Youtube</h1>
             <p className='youtube-description'>Corbininthemorning channel is about life and having a good time. <br /> Click <a href='https://www.youtube.com/@corbininthemorning'>here</a> to subscribe and watch the latest video</p>
-            <p className='youtube-video'>videos coming soon..</p>
+            
+            <h1 className='new-video'><a href='https://www.youtube.com/watch?v=B9HmPh5tUiM'>୨୧ Newest Video ୨୧</a></h1>
+            <section className='video-image'>
+                <img src={thumbnail} className='img-fluid' alt='youtube cover' style={{ width: '40em'}}></img>
+            </section>
 
             <p><a href={'/'} className='home-link'>⇒ Back to home ⇐</a></p>
         </>
